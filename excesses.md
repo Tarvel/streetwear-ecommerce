cb913d38782e4dc13dd85812e799b5a67d47999c
tarvel <taiyebabatundejames@gmail.com>
2025-07-02 17:12:56 +0100

    second commit

--- excesses.md
+++
@@ -1,5 +0,0 @@
-from base.html: <h1 class="text-5xl font-bold font-heading tracking-tight">Wimer</h1> 
-
-est
-
-
--- ztesting/test.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from django.utils.text import slugify
-
-
-base_slug = slugify("I am a boy")
-unique_id = uuid.uuid4().hex[:6]
-slug = f"{base_slug}-{unique_id}"
-
-print(slug)
